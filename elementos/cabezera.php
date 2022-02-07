63001dd6ab070f6d6b078046d5d336c0dd0fdbe7
nsalinas350 <nsalinas@unal.edu.co>
2022-02-07 02:23:46 -0500

    Update cabezera.php

--- elementos/cabezera.php
+++ elementos/cabezera.php
@@ -8,15 +8,6 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="../personas/personas.php">Personas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../facturas/facturas.php">Facturas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../busquedas/busquedas1.php">Busquedas</a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link" href="../usuarios/usuarios.php">Usuarios</a>
                 </li>
                 <li class="nav-item">
